ab6292c5e5ba4048105c42e0b513008e433bab0f
Abimanyu-TheProgrammer <jarfix01@gmail.com>
2020-11-29 22:58:58 +0700

    added w08.md, updated log, rank, and script

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-#This script can be used to copy files to my repo (now with multiple arguments and auto updates the SHASUM file)
+#This script can be used to copy files to my repo.
 
 
 for var in "$@"
 do
-       	cp $var /home/Abimanyu-TheProgrammer/git/os202/TXT/
-	echo "Copy of $var was successful."
+        cp $var /home/Abimanyu-TheProgrammer/git/os202/TXT/
+        echo "Copy of $var was successful."
 done
 echo "Transfer finished."
 cp sha.sh /home/Abimanyu-TheProgrammer/git/os202/TXT/
 cd git/os202/TXT/
-bash sha.sh 
+bash sha.sh
 rm -f sha.sh
-cd 
+cd
 echo "SHASUM256 updated."
+gitstat
