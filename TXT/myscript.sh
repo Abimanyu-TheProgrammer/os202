f1a61d3ca2b9a3276cf4dde9d5aa03957d5d4750
Abimanyu-TheProgrammer <jarfix01@gmail.com>
2020-12-13 01:37:20 +0700

    Added W10 top ten, myW10.tar.bz2.txt, W10 token

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-
-# This script can be used to copy files from the shared folder in debian to my account
-# it also changes the ownership of the file to Abimanyu-TheProgrammer
-
-for var in "$@"
-do
-	cp $var /home/Abimanyu-TheProgrammer/
-	chown Abimanyu-TheProgrammer /home/Abimanyu-TheProgrammer/$var
-done
-echo "Files moved and ownership changed"
+#!/usr/bin/bash
+# This script is used to tar and encrypt (armor) weekly assignment files
+filename=$1
+tar cvjf my$filename.tar.bz2 $filename
+gpg -o my$filename.tar.bz2.txt -e -a -r operatingsystems@vlsm.org -r jarfix01@gmail.com my$filename.tar.bz2 
